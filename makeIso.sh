0580a1ca5cd6a03b46fcc6ac3b2f4c37d7b2b2fd
bastlenoob <bastien.labouche96@orange.fr>
2018-05-28 11:56:53 +0200

    Test

--- makeIso.sh
+++ makeIso.sh
@@ -4,6 +4,7 @@
 iso=$1
 preseed=$2
 txt=$3
+#pt=$4
 
 # Définit dans quel dossier est exécuté le script
 local=`pwd`
@@ -17,14 +18,17 @@ cp $preseed $local/FichierIso/preseed/xubuntu.seed
 # Remplace le fichier isolinux.cfg par notre fichier
 cp $txt $local/FichierIso/isolinux/isolinux.cfg
 
+# Test
+#mkdir $local/FichierIso/Bitonio
+#echo "Vous allez finir dans le grand Pot-Au-Feu !" > $local/FichierIso/Bitonio/Pot-Au-Feu.txt
+#mkdir $local/FichierIso/Solibuntu
+#cp $pt $local/FichierIso/Solibuntu/pt.sh
 # Crée une image nommée "preseed_test.iso" à partir du dossier "FichierIso"
 cd $local/FichierIso
 genisoimage -r -J -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table -o preseed_test.iso .
+#mkisofs -J -r -v -o $local/preseed_test.iso -V Solibuntu -b isolinux/isolinux.bin -c isolinux/boot.cat -no-emul-boot -boot-load-size 4 -boot-info-table $local/FichierIso
 
-# Sort l'image du dossier "FichierIso"
-mv $local/FichierIso/preseed_test.iso $local/
-
-# Supprime le dossier "FichierIso devenu inutil"
+# Supprime le dossier "FichierIso devenu inutile"
 cd $local/
 rm -rf FichierIso/
 
