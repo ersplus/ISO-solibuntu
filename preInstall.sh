08c6b6e9407d583bd615d34d88d5e706d3741a15
bastlenoob <bastien.labouche96@orange.fr>
2018-05-31 10:32:15 +0200

    Tests

--- preInstall.sh
+++ preInstall.sh
@@ -1,4 +1,6 @@
 zenity --list --radiolist --width=500 \
 --text "Installer Solibuntu entrainera la perte de toutes vos données non-sauvegardées, voulez-vous continuer ?" \
---column "Sélectionner" --column "Options" TRUE "Oui" FALSE "Non" ; \
-if [ $? == "Non" ] ; then shutdown 0 ; fi ;
+--column "Sélectionner" --column "Options" TRUE "Oui" FALSE "Non"
+#if [ $? == "Non" ] ; then 
+#	shutdown 0 
+#fi
