513311720b06fb23a5c2edde8a8a48aa7bf9c2a0
bastlenoob <bastien.labouche96@orange.fr>
2018-06-01 17:11:49 +0200

    Modif message prévention

--- preInstall.sh
+++ preInstall.sh
@@ -1,7 +1,3 @@
-zenity --question --text="Installer Solibuntu entraînera la perte de toutes vos \
-données n'ayant pas été sauvegardées, désirez-vous toujours l'installer ?" \
---ok-label "Oui, continuer" --cancel-label="Non, arrêter l'installation"
-
-if [ $? == 1 ] ; then 
-	kill -9 -1
-fi
+zenity --info --text="Installer Solibuntu entraînera la perte de toutes vos \
+données n'ayant pas été sauvegardées, Si vous désirez annuler l'installer veuillez \
+retirer le média d'installation."
