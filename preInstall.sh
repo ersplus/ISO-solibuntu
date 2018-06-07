d634a52ce5711777e319ae2d77214896522a7f70
bastlenoob <bastien.labouche96@orange.fr>
2018-06-07 10:06:38 +0200

    Ajout licence démarrage

--- preInstall.sh
+++ preInstall.sh
@@ -1,3 +1,7 @@
-zenity --info --width=300 --height=200 --text="Installer Solibuntu entraînera la perte de toutes vos \
+zenity --info --width=500 --height=200 --text="Installer Solibuntu entraînera la perte de toutes vos \
 données n'ayant pas été sauvegardées, Si vous désirez annuler l'installer veuillez \
 retirer le média d'installation."
+
+zenity --info --width=500 --height=200 --text="En choisissant de poursuivre l'installation, vous \
+reconnaissez accepter la \"TrueType core fonts for the Web end user license agreement\" de \
+Microsoft disponible à cette adresse : http://corefonts.sourceforge.net/eula.htm"
